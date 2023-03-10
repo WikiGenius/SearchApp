a9e45f2603261c1dce0a0cad2b3b50c466d9f061
WikiGenius <muhammedelyamani92@gmail.com>
2023-03-10 22:56:07 +0200

    Update uix

--- utils/layout.py
+++ utils/layout.py
@@ -4,7 +4,12 @@ from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import Screen
 from kivy.core.window import Window
 from kivy.uix.floatlayout import FloatLayout
+from kivymd.uix.slider import MDSlider
 
+class Myslider(MDSlider):
+    def on_touch_up(self, touch):
+        self.active = True
+        
 # Define a custom widget for loading files
 class LoadFile(FloatLayout):
     load = ObjectProperty(None)
